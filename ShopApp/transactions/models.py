152c68f5e587dea53ddc0c082ec33d898ff44128
Shixuan Li <lishixuan001@berkeley.edu>
2020-11-05 17:52:31 +0800

    part result

--- ShopApp/transactions/models.py
+++ ShopApp/transactions/models.py
@@ -1,19 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
-from django.utils import timezone
+from django.utils.timezone import now
 
 
-class Product(models.Model):
-    def __str__(self):
-        return "{name}".format(name=self.name)
-
-    def update(self):
-        self.sufficient = self.stock > self.limit
-
-    limit = models.IntegerField(default=5)
-
+class Person(models.Model):
     name = models.CharField(default=None, null=True, blank=True, max_length=100)
-    idx = models.CharField(default=None, null=True, blank=True, max_length=100)
-    price = models.FloatField(default=None, null=True, blank=True)
-    stock = models.IntegerField(default=None, null=True, blank=True)
-    sufficient = models.BooleanField(default=True)
+    time = models.DateTimeField(default=now, editable=False)
+    image = models.ImageField(upload_to='images/')
--- ShopApp/transactions/urls.py
+++ ShopApp/transactions/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('registration/', views.registration, name='registration'),
-    path('login/', views.login, name='login'),
-    path('product_details/<str:product_idx>', views.product_details, name='product_details'),
-    path('logout/', views.logout, name='logout'),
-    path('merchandise/', views.merchandise, name='merchandise'),
 ]
