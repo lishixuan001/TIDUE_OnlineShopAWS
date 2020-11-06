74346a73850d0ce98831f65187d759c173116fc2
Shixuan Li <lishixuan001@berkeley.edu>
2020-11-06 12:32:29 +0800

    remove screeenshot

--- ShopApp/transactions/models.py
+++ ShopApp/transactions/models.py
@@ -5,4 +5,4 @@ from django.utils.timezone import now
 class Person(models.Model):
     name = models.CharField(default=None, null=True, blank=True, max_length=100)
     time = models.DateTimeField(default=now, editable=False)
-    image = models.ImageField(upload_to='images/')
+    # image = models.ImageField(upload_to='images/')
--- ShopApp/transactions/views.py
+++ ShopApp/transactions/views.py
@@ -8,16 +8,17 @@ def index(request):
 
     if request.method == 'POST':
         name = request.POST.get('name')
-        image = request.FILES['image']
-
-        if not (image.name.endswith("png") or image.name.endswith("jpg")):
-            context["file_error"] = True
-            return render(request, 'new_car.html', context=context)
+        # image = request.FILES['image']
+        #
+        # if not (image.name.endswith("png") or image.name.endswith("jpg")):
+        #     context["file_error"] = True
+        #     return render(request, 'new_car.html', context=context)
 
         if Person.objects.filter(name=name).exists():
             context["submit_error"] = True
         else:
-            Person.objects.create(name=name, image=image)
+            # Person.objects.create(name=name, image=image)
+            Person.objects.create(name=name)
             context["submit_success"] = True
 
         return render(request, 'index.html', context=context)
